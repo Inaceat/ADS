e337905374e02fe55fc6999f0d31d6b90654326f
Inaceat <inaceat@gmail.com>
2020-03-17 13:42:45 +0300

    Initial commit, one empty project for Lab1.

---
+++ Lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Program();
+            x.Run();
+        }
+
+        private void Run()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
