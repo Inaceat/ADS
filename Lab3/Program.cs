e2f07f88ff4335548bce92764b10ec6c6db98bc0
Inaceat <inaceat@gmail.com>
2020-03-20 15:57:42 +0300

    [3-2]Some experiments, mostly working, awfully looking.

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,10 +1,79 @@
-﻿namespace Lab3
+﻿using System;
+
+namespace Lab3
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void DoStockExchangeCheating()
         {
 
+        }
+
+        private static void DoMaxFind()
+        {
+            var finder = new ExpressionMaxFinder
+            (
+                new[] {1, 2, -4, 5}, 
+                new[] {'+', '*', '+'}
+            );
+
+            finder.Find();
+            
+            Console.WriteLine("{0} = {1}", finder.Expression(), finder.Result());
+        }
+        
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var choice = 2;/*ReadVariantFromConsole(new[]
+                {
+                    "Exit",
+                    "Stock exchange cheat",
+                    "Expression max"
+                });*/
+
+                switch (choice)
+                {
+                case 0:
+                    return;
+
+                case 1:
+                    DoStockExchangeCheating();
+                    break;
+
+                case 2:
+                    DoMaxFind();
+                    return;//break;
+                }
+            }
+        }
+
+        private static int ReadVariantFromConsole(string[] variants)
+        {
+            while (true)
+            {
+                for (var i = 0; i < variants.Length; ++i)
+                    Console.WriteLine("{0}. {1}", i + 1, variants[i]);
+
+                var userInput = Console.ReadLine();
+                int choice;
+                try
+                {
+                    choice = int.Parse(userInput);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Wrong input, try again");
+                    continue;
+                }
+
+                if (0 < choice && choice <= variants.Length)
+                    return choice - 1;
+
+                Console.WriteLine("Wrong input, try again");
+            }
         }
     }
 }
