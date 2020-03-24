48f008136039e8d6d6ea3ab8f99c75c1c6a36511
inaceat <inaceat@gmail.com>
2020-03-25 00:48:00 +0300

    [3-1][WIP]Suubarray max finder started.

--- Lab3/StockExchangeCheater.cs
+++ Lab3/StockExchangeCheater.cs
@@ -1,29 +1,52 @@
-﻿namespace Lab3
+﻿using System;
+
+namespace Lab3
 {
     public class StockExchangeCheater
     {
-        private int[] _prices;
+        private readonly int[] _prices;
+
+        private readonly int[] _differences;
 
         public StockExchangeCheater(uint[] prices)
         {
-            _prices = new int[prices.Length];
+            if (null == prices)
+                throw new ArgumentNullException();
 
+
+
+            _prices = new int[prices.Length];
+            
             for (var i = 0; i < prices.Length; ++i)
                 _prices[i] = (int) prices[i];
+
+
+            //To one cycle?
+            _differences = new int[prices.Length - 1];
+
+            for (var i = 0; i < prices.Length - 1; ++i)
+                _differences[i] = _prices[i + 1] - _prices[i];   
         }
 
         public (int buyDayIndex, int sellDayIndex, int profit) DoTimeWarpCheating()
         {
-            var differences = new int[_prices.Length - 1];
+            (int buyDayIndex, int sellDayIndex) = GetMaxSumSubarray(0, _differences.Length - 1);
+            ++sellDayIndex;
 
-            for (var i = 0; i < differences.Length; ++i)
-                differences[i] = _prices[i + 1] - _prices[i];
+            return (buyDayIndex, sellDayIndex, _prices[sellDayIndex] - _prices[buyDayIndex]);
+        }
+
+
+        private (int subBeginIndex, int subEndIndex) GetMaxSumSubarray(int arrayBeginIndex, int arrayEndIndex)
+        {
+            if (arrayBeginIndex == arrayEndIndex)
+                return (arrayBeginIndex, arrayEndIndex);
+
+            int afterMidIndex = (arrayEndIndex - arrayBeginIndex + 1) / 2;
 
             
-            
 
-
-            return (0, 0, 0);
+            return (0, 0);
         }
     }
 }
