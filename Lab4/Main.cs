69a5909094f7e6f8a738b1af48e40dec3c15b201
inaceat <inaceat@gmail.com>
2020-03-31 21:32:24 +0300

    [4]Selection sort added.

--- Lab4/Main.cs
+++ Lab4/Main.cs
@@ -11,15 +11,34 @@ namespace Lab4
             var bench = new SortingTester(42, 10, 100, 1000, 10000);
 
 
+            TestNonSort(bench);
+            TestSelectionSort(bench);
+
+        }
+
+
+        private static void TestNonSort(SortingTester bench)
+        {
             var nonSorter = new NonSorter();
 
             var results = bench.Test("NonSorter", nonSorter);
 
             Console.WriteLine("NonSorter:");
+            
             foreach (var result in results)
-            {
                 result.PrintToConsole();
-            }
+        }
+
+        private static void TestSelectionSort(SortingTester bench)
+        {
+            var selectionSorter = new SelectionSorter();
+
+            var results = bench.Test("SelectionSorter", selectionSorter);
+
+            Console.WriteLine("SelectionSorter:");
+            
+            foreach (TestResult result in results)
+                result.PrintToConsole();
         }
     }
 }
---
+++ Lab4/Sorters/SelectionSorter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.Sorters
+{
+    class SelectionSorter : IIntegerArraySorter
+    {
+        public void Sort(int[] data)
+        {
+            if (null == data)
+                return;
+
+            
+            int size = data.Length;
+
+            if (size < 2)
+                return;
+
+
+            unsafe
+            {
+                //C++ style addresses: [begin; end) arrays
+                fixed (int* begin = &data[0])
+                {
+                    int* end = begin + size;
+
+                    
+                    for (int* current = begin; current < end-1; ++current)
+                    {
+                        //Find min elemennt in non-sorted part of array
+                        int* min = current;
+
+                        for (int* possibleMin = current; possibleMin < end; ++possibleMin)
+                        {
+                            if (*possibleMin < *min)
+                                min = possibleMin;
+                        }
+
+                        //Swap min with current
+                        int tmp = *min;
+                        *min = *current;
+                        *current = tmp;
+                    }
+                }
+            }
+            
+        }
+        
+
+        public (int swaps, int compares) SortAndCountSwapsAndCompares(int[] data)
+        {
+            if (null == data)
+                return (0, 0);
+
+            
+            int size = data.Length;
+
+            if (size < 2)
+                return (0, 0);
+
+            int swaps = 0;
+            int compares = 0;
+
+            unsafe
+            {
+                //C++ style addresses: [begin; end) arrays
+                fixed (int* begin = &data[0])
+                {
+                    int* end = begin + size;
+
+                    
+                    for (int* current = begin; current < end-1; ++current)
+                    {
+                        //Find min elemennt in non-sorted part of array
+                        int* min = current;
+
+                        for (int* possibleMin = current; possibleMin < end; ++possibleMin)
+                        {
+                            compares++;
+                            if (*possibleMin < *min)
+                                min = possibleMin;
+                        }
+
+                        //Swap min with current
+                        swaps++;
+
+                        int tmp = *min;
+                        *min = *current;
+                        *current = tmp;
+                    }
+                }
+            }
+
+            return (swaps, compares);
+        }
+    }
+}
