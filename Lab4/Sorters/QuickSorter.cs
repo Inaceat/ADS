f83d4dd840e3e960b7bbe35f3b960e17f6a52190
inaceat <inaceat@gmail.com>
2020-04-30 22:54:54 +0300

    [4]Quick sorter implemented.

--- Lab4/Sorters/QuickSorter.cs
+++ Lab4/Sorters/QuickSorter.cs
@@ -22,15 +22,72 @@ namespace Lab4.Sorters
 
             unsafe
             {
-                //C++ style addresses: [begin; end) arrays
+                //[first; last], INCLUSIVE arrays
                 fixed (int* begin = &data[0])
                 {
-                    
+                    int* end = begin + size - 1;
+
+                    DoQuickSort(begin, end);
                 }
             }
             
         }
-        
+
+
+        private unsafe void SwapData(int* left, int* right)
+        {
+            int tmp = *left;
+            *left = *right;
+            *right = tmp;
+        }
+
+
+        private unsafe void DoQuickSort(int* begin, int* end)
+        {
+            //If size < 2
+            if (end - begin < 1)
+                return;
+
+            //If size == 2
+            if (end - begin == 1)
+            {
+                if (*end < *begin)
+                    SwapData(begin, end);
+
+                return;
+            }
+
+
+            int midValue = (int)(((long)*begin + *end + *(begin + (end - begin) / 2)) / 3);
+
+            int* left = begin;
+            int* right = end;
+
+            while (true)
+            {
+                while (*left < midValue && left < right)
+                    ++left;
+
+                while (midValue <= *right && left < right)
+                    --right;
+
+                if (left < right)
+                    SwapData(left, right);
+                else
+                    break;
+            }
+
+            //Now left == right
+            if (*left < midValue)
+                ++right;
+            else
+                --left;
+
+
+            DoQuickSort(begin, left);
+            DoQuickSort(right, end);
+        }
+
 
         public (int swaps, int compares) SortAndCountSwapsAndCompares(int[] data)
         {
@@ -48,14 +105,83 @@ namespace Lab4.Sorters
 
             unsafe
             {
-                //C++ style addresses: [begin; end) arrays
+                //[first; last], INCLUSIVE arrays
                 fixed (int* begin = &data[0])
                 {
-                    
+                    int* end = begin + size - 1;
+
+                    DoQuickSortAndCount(begin, end, ref swaps, ref compares);
                 }
             }
 
             return (swaps, compares);
+        }
+
+
+        private unsafe void DoQuickSortAndCount(int* begin, int* end, ref int swaps, ref int compares)
+        {
+            //If size < 2
+            if (end - begin < 1)
+                return;
+
+            //If size == 2
+            if (end - begin == 1)
+            {
+                ++compares;
+                if (*end < *begin)
+                {
+                    ++swaps;
+                    SwapData(begin, end);
+                }
+                    
+
+                return;
+            }
+
+
+            int midValue = (int)(((long)*begin + *end + *(begin + (end - begin) / 2)) / 3);
+
+            int* left = begin;
+            int* right = end;
+
+            while (true)
+            {
+                while (*left < midValue && left < right)
+                {
+                    compares += 2;//for each compare in cycle condition check
+                    ++left;
+                }
+                ++compares;//For failed cycle condition check
+
+
+                while (midValue <= *right && left < right)
+                {
+                    compares += 2;
+                    --right;
+                }
+                ++compares;
+
+
+                ++compares;
+                if (left < right)
+                {
+                    ++swaps;
+                    SwapData(left, right);
+                }
+                else
+                    break;
+            }
+
+            //Now left == right
+            compares++;
+            if (*left < midValue)
+                ++right;
+            else
+                --left;
+
+
+            DoQuickSortAndCount(begin, left, ref swaps, ref compares);
+            DoQuickSortAndCount(right, end, ref swaps, ref compares);
         }
     }
 }
